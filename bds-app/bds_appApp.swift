4497e966c7b45c51743a9559d7c873afc46efbf6
sahmed9123 <sahmed21@umd.edu>
2023-11-01 20:24:46 -0400

    Initial Commit

---
+++ bds-app/bds_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bds_appApp.swift
+//  bds-app
+//
+//  Created by Saima Ahmed on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct bds_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
